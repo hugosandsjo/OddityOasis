9390b0b32ac32b62dcb7856c36b391d21b6b02a0
Julia <jsgoransson@gmail.com>
2024-04-22 10:47:28 +0200

    added bored API

--- OddityOasis/src/App.jsx
+++ OddityOasis/src/App.jsx
@@ -3,21 +3,29 @@ import FactBox from "./components/FactBox";
 import Header from "./components/Header";
 import Button from "./components/Button";
 import ButtonContainer from "./components/ButtonContainer";
+import Bored from "./components/Bored";
 
 import "./App.css";
 
 function App() {
-  const [selectedFact, setSelectedFact] = useState("random");
+  const [selectedFact, setSelectedFact] = useState("daily");
+  const [randomClickCount, setRandomClickCount] = useState(0);
+
+  const handleRandomButtonClick = () => {
+    setSelectedFact("random");
+    setRandomClickCount((prevCount) => prevCount + 1);
+  };
 
   return (
     <>
       <Header />
       <ButtonContainer>
-        <Button text="Random fact" onClick={() => setSelectedFact("random")} />
+        <Button text="Random fact" onClick={handleRandomButtonClick} />
         <Button text="Today's fact" onClick={() => setSelectedFact("daily")} />
         <Button text="All facts" />
       </ButtonContainer>
       <FactBox selectedFact={selectedFact} />
+      {randomClickCount >= 3 && <Bored />}
     </>
   );
 }
---
+++ OddityOasis/src/components/Bored.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+
+function Bored() {
+  const [activity, setActivity] = useState(null);
+  const [selectedType, setSelectedType] = useState("");
+
+  async function fetchActivity(type) {
+    try {
+      let url = "https://www.boredapi.com/api/activity";
+      if (type) {
+        url += `?type=${type}`;
+      }
+
+      const response = await fetch(url);
+      const data = await response.json();
+      setActivity(data.activity);
+    } catch {
+      console.log("error");
+    }
+  }
+
+  useEffect(() => {
+    fetchActivity(selectedType);
+  }, [selectedType]);
+
+  const handleTypeChange = (event) => {
+    setSelectedType(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Feeling Bored?</h1>
+      <p>Pick an activity type:</p>
+      <select value={selectedType} onChange={handleTypeChange}>
+        <option value="">All</option>
+        <option value="education">Education</option>
+        <option value="recreational">Recreational</option>
+      </select>
+      <p>Here's an idea:</p>
+      <p>{activity}</p>
+    </div>
+  );
+}
+
+export default Bored;
