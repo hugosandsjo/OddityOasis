b43cf852c4033782b88e22aca7846eebfa53ecdd
hugosandsjo <hugosandsjo@gmail.com>
2024-09-11 11:05:09 +0200

    added comment

--- src/components/BoredBox.jsx
+++ src/components/BoredBox.jsx
@@ -70,6 +70,8 @@ function BoredBox() {
   const [isClosed, setIsClosed] = useState(false);
   const [isRotated, setIsRotated] = useState(false);
 
+  // need to update the api
+
   async function fetchActivity(type) {
     try {
       let url = "https://www.boredapi.com/api/activity";
